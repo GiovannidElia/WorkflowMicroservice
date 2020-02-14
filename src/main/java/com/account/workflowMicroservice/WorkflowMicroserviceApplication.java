2574c4317f56f7d6b68406a29724661b34a5e3c0
GiovannidElia <delia.gio@gmail.com>
2020-02-14 16:28:41 +0100

    Initial commit

---
+++ src/main/java/com/account/workflowMicroservice/WorkflowMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.account.workflowMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/account/workflowMicroservice/WorkflowMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.account.workflowMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
